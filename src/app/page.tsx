5eef863387057c0791190c3f4ae90cf0762befb3
BBryanJ <boeing.bryan242@gmail.com>
2024-12-07 14:10:32 -0500

    TailwindCSS installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h2>Welcome to the library</h2>
+      <h2 className='text-3xl font-bold underline'>Welcome to the library</h2>
     </div>
   );
 }
