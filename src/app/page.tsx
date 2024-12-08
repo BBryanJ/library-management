bb958c4e3ef2b4db94035cc38e4a7e2800ef1fcc
BBryanJ <boeing.bryan242@gmail.com>
2024-12-08 13:29:23 -0500

    Create homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+import Link from 'next/link';
+
 export default function HomePage() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center gap-4'>
       <h2 className='text-3xl font-bold underline'>Welcome to the library</h2>
+      <div className='grid grid-cols-2 gap-4'>
+        <Link href='/books'>
+          <div className='px-6 py-4 text-center bg-red-400 cursor-pointer hover:bg-red-500'>
+            <h3 className='text-2xl font-bold underline'>
+              Browse Available Books
+            </h3>
+          </div>
+        </Link>
+        <Link href='/add-book'>
+          <div className='px-6 py-4 text-center bg-blue-400 cursor-pointer hover:bg-blue-500'>
+            <h3 className='text-2xl font-bold underline'>Add a Book</h3>
+          </div>
+        </Link>
+        <Link href='/search'>
+          <div className='px-6 py-4 text-center bg-green-400 cursor-pointer hover:bg-green-500'>
+            <h3 className='text-2xl font-bold underline'>Search Books</h3>
+          </div>
+        </Link>
+        <Link href='/returns'>
+          <div className='px-6 py-4 text-center bg-yellow-400 cursor-pointer hover:bg-yellow-500'>
+            <h3 className='text-2xl font-bold underline'>Return Books</h3>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
