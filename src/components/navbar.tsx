d034548c8c714f4b018921a167aa7cc5d26bcbe3
Bryan Boeing <boeing.bryan242@gmail.com>
2024-12-09 19:35:18 -0500

    Navbar fixes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,14 +3,14 @@ import Link from 'next/link';
 // Renders a navigation bar with links to different pages
 export default function Navbar() {
   return (
-    <nav className='flex flex-wrap items-center justify-between w-full max-w-screen-lg px-4 mx-auto basis-full'>
+    <nav className='flex flex-wrap items-center justify-between w-full max-w-screen-xl px-4 mr-auto basis-full'>
       <Link
         href='/'
         className='flex-none text-xl font-semibold text-gray-600 hover:text-gray-400'
       >
         Library Management
       </Link>
-      <div className='flex items-center gap-x-4'>
+      <div className='flex items-center justify-between flex-1 w-auto gap-x-4'>
         <Link
           href='/books'
           className='font-medium text-gray-600 hover:text-gray-400'
